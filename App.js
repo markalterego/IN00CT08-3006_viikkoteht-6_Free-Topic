46f9532520dee299363b33941ef07201e982dd46
markalterego <122254234+markalterego@users.noreply.github.com>
2025-02-17 16:00:07 +0200

    assignment done

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Button, StyleSheet, Text, View } from 'react-native';
+
+const URL = 'https://randomfox.ca/floof/?ref=apilist.fun';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [data, setData] = useState({}); // data from fetch
+
+    function pollFox() {
+        fetch(URL)
+        .then(response => response.json())
+        .then((json) => {
+            setData(json);
+            console.log(json);   
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    return (
+        <View style={styles.topContainer}>
+            <View style={styles.bottomContainer}>
+                <Text style={styles.header}>{'Dare to press the button?'}</Text>
+                <Button 
+                    title='Press me'
+                    onPress={pollFox}
+                />
+                {
+                data.image && 
+                <Image 
+                    style={styles.image} 
+                    source={{ uri: data.image }} 
+                    resizeMode='contain'
+                />
+                }
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    topContainer: { // trying to preserve backgroundcolor by adding top and bottom containers
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    bottomContainer: {
+        width: '80%',
+        height: '90%',
+        top: '5%',
+    },
+    header: { 
+        textAlign: 'center',
+        fontSize: 40,
+    },
+    image: {
+        width: '100%',
+        height: 300
+    }
 });
